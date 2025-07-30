4162eeb1db9d72524621381af1bc184c8ff4d1cb
Coder Hillary <coderhillary@Coders-MBP.mshome.net>
2025-07-30 08:51:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
+  const [goals, setGoals] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/goals")
+      .then((r) => r.json())
+      .then(setGoals);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <NavBar />
+      <Outlet context={{ goals, setGoals }} />
     </div>
   );
 }
